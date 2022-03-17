c532ba59d65460c0933b7c7db626da6d038a13c8
douevenfeel <mdxnilovd@gmail.com>
2022-03-17 19:09:17 +0300

    lab4

--- lab4/1.js
+++ lab4/1.js
@@ -7,6 +7,27 @@
  */
 function getAnagramms(arr) {
     //code here
+    let newArr = Array()
+    let arrSort = Array()
+    let arrWords = Array()
+    let tempArr
+    let tempWord
+    for (let i = 0; i < arr.length; i++) {
+        tempWord = arr[i].toLowerCase().split('').sort().join('')
+        if (arrSort.includes(tempWord) == false) {
+            arrWords.push(arr[i])
+            arrSort.push(tempWord)
+        }
+    }
+    for (let i = 0; i < arrWords.length; i++) {
+        tempArr = []
+        tempArr.push(arrWords[i])
+        for (let j = i + 1; j < arr.length; j++) {
+            ((arr[j].toLowerCase().split('').sort().join('') == arrSort[i]) && (tempArr.includes(arr[j]) == false)) ? tempArr.push(arr[j]): false;
+        }
+        newArr.push(tempArr)
+    }
+    return newArr
 }
 
 module.exports = getAnagramms;
--- lab4/5.js
+++ lab4/5.js
@@ -12,6 +12,24 @@
 
 function checkBrackets(str) {
     //code here
+    str = str.split('')
+    let checking = Array()
+    for (let i = 0, j = 0; i < str.length; i++) {
+        if (str[i] == '[' || str[i] == '(' || str[i] == '<') {
+            checking.push(str[i])
+            j++
+        } else if (
+            (str[i] == ']' && (checking[j - 1] == '[')) ||
+            (str[i] == ')' && (checking[j - 1] == '(')) ||
+            (str[i] == '>' && (checking[j - 1] == '<'))
+        ) {
+            checking.pop()
+            j--
+        } else {
+            return false
+        }
+    }
+    return((checking.length == 0)? true : false)
 }
 
 module.exports = checkBrackets;
