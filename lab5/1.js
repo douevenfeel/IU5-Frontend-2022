3b2e244cc9d71b1afe487243ebabbaac027f1f0d
douevenfeel <mdxnilovd@gmail.com>
2022-03-27 13:40:21 +0300

    lab5

--- lab5/1.js
+++ lab5/1.js
@@ -6,3 +6,6 @@
  * [1,2,3].myMap((x) => x*2) -> [2,4,6]
  * Нужно назвать myMap !!!!!
  */
+Array.prototype.myMap = function (callback) {
+    return this.reduce((arr, current) => arr.concat(callback(current)), []);
+};
